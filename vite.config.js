1c53333636dbb125c1d9fdd260139e87d4ef986c
duro06 <andi.meka.025@gmail.com>
2025-02-26 22:07:49 +0700

    awal

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { fileURLToPath, URL } from 'url';
+
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: './'
+});
