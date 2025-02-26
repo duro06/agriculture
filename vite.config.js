dc9f866abf23c1a48544a6ddcefa5ac2def91009
duro06 <andi.meka.025@gmail.com>
2025-02-26 22:24:35 +0700

    kembali ke dasar

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  server: {
-    port: 3000
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: './'
-});
+  plugins: [vue()],
+})
