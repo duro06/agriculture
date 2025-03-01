8d431731af6311465e665dd80f8c345202c8cada
duro06 <andi.meka.025@gmail.com>
2025-03-01 22:04:10 +0700

    logo udim

---
+++ src/components/app/AvatarLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-avatar :size="size">
+    <img src="~assets/icon.svg">
+  </q-avatar>
+</template>
+
+<script setup>
+defineProps({
+  size: {
+    type: String,
+    default: 'lg'
+  }
+})
+</script>
